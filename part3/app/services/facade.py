01c95c8561901ca4e6b244627ada41f6d580e3ee
kamisos3 <kamilasostre11@gmail.com>
2025-07-17 13:27:54 -0400

    Switching to SQLAlchemyRepository in facade

--- part3/app/services/facade.py
+++ part3/app/services/facade.py
@@ -3,14 +3,14 @@ from app.models.user import User
 from app.models.place import Place
 from app.models.review import Review
 from app.models.amenity import Amenity
-from app.persistance.repository import InMemoryRepository
+from app.persistance.repository import SQLAlchemyRepository
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
+        self.user_repo = SQLAlchemyRepository(User)
+        self.place_repo = SQLAlchemyRepository(Place)
+        self.review_repo = SQLAlchemyRepository(Review)
+        self.amenity_repo = SQLAlchemyRepository(Amenity)
 
     def create_place(self, place_data):
         title = place_data.get("title")

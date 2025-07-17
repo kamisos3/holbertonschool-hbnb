40bf37cf7ce73bba19fcd0a5ae17e64875da7dad
kamisos3 <kamilasostre11@gmail.com>
2025-07-17 18:34:14 -0400

    Making repository user in a different file

--- part3/app/services/facade.py
+++ part3/app/services/facade.py
@@ -3,7 +3,7 @@ from app.models.user import User
 from app.models.place import Place
 from app.models.review import Review
 from app.models.amenity import Amenity
-from app.persistence.repository import SQLAlchemyRepository
+from app.persistance.repository import SQLAlchemyRepository
 
 class HBnBFacade:
     def __init__(self):
